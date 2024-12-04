877827177e17775c587c7b5951ca846ae309501e
yuyahy <xvto.ya@gmail.com>
2024-12-04 11:42:09 +0000

    feat: 第一章完了

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # test-driven-development
 [テスト駆動開発](https://www.amazon.co.jp/%E3%83%86%E3%82%B9%E3%83%88%E9%A7%86%E5%8B%95%E9%96%8B%E7%99%BA-Kent-Beck/dp/4274217884)の成果物を管理するリポジトリ
 
+# TODO
+- [] Go言語で写経？
+- [] CI/CD作る？
---
+++ go/src/money_test.go
@@ -0,0 +1,21 @@
+package money
+
+import "testing"
+
+// TODOリスト
+// - $5 + 10 CHF = $10（レートが2:1の場合）
+// - (完了)$5 * 2 = $10
+// - Amountをprivateにする
+// - Dollarの副作用どうする？
+// - Moneyの丸め処理どうする？
+
+func TestMultiplication(t *testing.T) {
+	// 今後の章でコンストラクタをカスタムする可能性があるので、リテラル構文ではなく、
+	// 念の為コンストラクタ関数で実装
+	// five := &Dollar{5}
+	five := NewDollar(5)
+	five.Times(2)
+	if five.Amount != 10 {
+		t.Errorf("Expected 10, but got %d", five.Amount)
+	}
+}
