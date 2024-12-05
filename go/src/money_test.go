36d1d72dbbb7b5a48b4eec61cfd5ee9d19c53bdb
yuyahy <xvto.ya@gmail.com>
2024-12-05 21:08:20 +0900

    feat: 第2章完了 (#2)
    * feat: 第2章完了
    
    * ci: Linter Workflowを追加

--- go/src/money_test.go
+++ go/src/money_test.go
@@ -6,7 +6,7 @@ import "testing"
 // - $5 + 10 CHF = $10（レートが2:1の場合）
 // - (完了)$5 * 2 = $10
 // - Amountをprivateにする
-// - Dollarの副作用どうする？
+// - (完了)Dollarの副作用どうする？
 // - Moneyの丸め処理どうする？
 
 func TestMultiplication(t *testing.T) {
@@ -14,8 +14,12 @@ func TestMultiplication(t *testing.T) {
 	// 念の為コンストラクタ関数で実装
 	// five := &Dollar{5}
 	five := NewDollar(5)
-	five.Times(2)
-	if five.Amount != 10 {
+	product := five.Times(2)
+	if product.Amount != 10 {
 		t.Errorf("Expected 10, but got %d", five.Amount)
+	}
+	product = five.Times(3)
+	if product.Amount != 15 {
+		t.Errorf("Expected 15, but got %d", five.Amount)
 	}
 }
