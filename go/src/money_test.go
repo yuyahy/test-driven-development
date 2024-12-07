c6d3b0baf8c1d643a7a15a959b44909fcaa60482
yuyahy <xvto.ya@gmail.com>
2024-12-07 17:00:51 +0900

    feat: 第6章完了 (#6)

--- go/src/money_test.go
+++ go/src/money_test.go
@@ -14,37 +14,44 @@ import "testing"
 // - 他オブジェクトとの等価性比較
 // - ~~5CHF * 2 = 10CHF~~
 // - DollarとFrancの重複
-// - Equalsの一般化
+// - ~~Equalsの一般化~~
 // - Timesの一般化
+// - FrancとDollarを比較する
 
 func TestMultiplication(t *testing.T) {
 	// 今後の章でコンストラクタをカスタムする可能性があるので、リテラル構文ではなく、
 	// 念の為コンストラクタ関数で実装
 	// five := &Dollar{5}
 	five := NewDollar(5)
-	if !NewDollar(10).Equals(five.Times(2)) {
+	if !NewDollar(10).Equals(five.Times(2).Money) {
 		t.Errorf("Expected 10, but got %d", five.amount)
 	}
-	if !NewDollar(15).Equals(five.Times(3)) {
+	if !NewDollar(15).Equals(five.Times(3).Money) {
 		t.Errorf("Expected 15, but got %d", five.amount)
 	}
 }
 
 func TestEquality(t *testing.T) {
-	if !NewDollar(5).Equals(NewDollar(5)) {
+	if !NewDollar(5).Equals(NewDollar(5).Money) {
 		t.Errorf("Expected equal, but got not")
 	}
-	if NewDollar(5).Equals(NewDollar(6)) {
+	if NewDollar(5).Equals(NewDollar(6).Money) {
+		t.Errorf("Expected not equal, but got equal")
+	}
+	if !NewFranc(5).Equals(NewFranc(5).Money) {
+		t.Errorf("Expected equal, but got not")
+	}
+	if NewFranc(5).Equals(NewFranc(6).Money) {
 		t.Errorf("Expected not equal, but got equal")
 	}
 }
 
 func TestFrancMultiplication(t *testing.T) {
 	five := NewFranc(5)
-	if !NewFranc(10).Equals(five.Times(2)) {
+	if !NewFranc(10).Equals(five.Times(2).Money) {
 		t.Errorf("Expected 10, but got %d", five.amount)
 	}
-	if !NewFranc(15).Equals(five.Times(3)) {
+	if !NewFranc(15).Equals(five.Times(3).Money) {
 		t.Errorf("Expected 15, but got %d", five.amount)
 	}
 }
