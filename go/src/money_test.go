7c0291660396c22568b4906365533ff722a323f3
yuyahy <xvto.ya@gmail.com>
2024-12-07 02:24:45 +0000

    feat: 第5章完了

--- go/src/money_test.go
+++ go/src/money_test.go
@@ -4,14 +4,18 @@ import "testing"
 
 // TODOリスト
 // - $5 + 10 CHF = $10（レートが2:1の場合）
-// - (完了)$5 * 2 = $10
-// - (完了)Amountをprivateにする
-// - (完了)Dollarの副作用どうする？
+// - ~~$5 * 2 = $10~~
+// - ~~Amountをprivateにする~~
+// - ~~Dollarの副作用どうする？~~
 // - Moneyの丸め処理どうする？
-// - (完了)equals()
+// - ~~equals()~~
 // - hashCode()
 // - nullとの等価性比較
 // - 他オブジェクトとの等価性比較
+// - ~~5CHF * 2 = 10CHF~~
+// - DollarとFrancの重複
+// - Equalsの一般化
+// - Timesの一般化
 
 func TestMultiplication(t *testing.T) {
 	// 今後の章でコンストラクタをカスタムする可能性があるので、リテラル構文ではなく、
@@ -32,5 +36,15 @@ func TestEquality(t *testing.T) {
 	}
 	if NewDollar(5).Equals(NewDollar(6)) {
 		t.Errorf("Expected not equal, but got equal")
+	}
+}
+
+func TestFrancMultiplication(t *testing.T) {
+	five := NewFranc(5)
+	if !NewFranc(10).Equals(five.Times(2)) {
+		t.Errorf("Expected 10, but got %d", five.amount)
+	}
+	if !NewFranc(15).Equals(five.Times(3)) {
+		t.Errorf("Expected 15, but got %d", five.amount)
 	}
 }
