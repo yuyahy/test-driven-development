2337608b633b2e7b222f83880ee553f0a7988b67
yuyahy <xvto.ya@gmail.com>
2024-12-07 07:58:56 +0000

    feat: 第6章完了

--- go/src/dollar.go
+++ go/src/dollar.go
@@ -1,23 +1,15 @@
 package money
 
 type Dollar struct {
-	amount int
+	Money // Goは継承の概念がないので、compositionで表現
 }
 
 func NewDollar(amount int) Dollar {
 	return Dollar{
-		amount: amount,
+		Money: Money{amount: amount},
 	}
 }
 
 func (d Dollar) Times(multiplier int) Dollar {
 	return NewDollar(d.amount * multiplier)
-}
-
-// Goのstructは"=="演算子で各フィールドを比較可能だが、サンプルでも使用されているJavaでは
-// "=="とequals()は異なる意味を持つ場合があるので(e.g. stringでは前者は参照先の比較、後者は値の比較)、
-// 今回はEquals()で等価性を比較するコードにしている
-func (d Dollar) Equals(object any) bool {
-	dollar := object.(Dollar)
-	return d.amount == dollar.amount
 }
--- go/src/franc.go
+++ go/src/franc.go
@@ -1,23 +1,15 @@
 package money
 
 type Franc struct {
-	amount int
+	Money // Goは継承の概念がないので、compositionで表現
 }
 
 func NewFranc(amount int) Franc {
 	return Franc{
-		amount: amount,
+		Money{amount: amount},
 	}
 }
 
 func (d Franc) Times(multiplier int) Franc {
 	return NewFranc(d.amount * multiplier)
-}
-
-// Goのstructは"=="演算子で各フィールドを比較可能だが、サンプルでも使用されているJavaでは
-// "=="とequals()は異なる意味を持つ場合があるので(e.g. stringでは前者は参照先の比較、後者は値の比較)、
-// 今回はEquals()で等価性を比較するコードにしている
-func (f Franc) Equals(object any) bool {
-	franc := object.(Franc)
-	return f.amount == franc.amount
 }
