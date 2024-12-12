36b4c7757aee19d9c834c316035004d46ba699ab
yuyahy <xvto.ya@gmail.com>
2024-12-13 07:57:22 +0900

    feat: 第16章完了 (#16)

--- go/src/expression.go
+++ go/src/expression.go
@@ -1,6 +1,7 @@
 package money
 
 type Expression interface {
+	Times(multiplier int) Expression
 	Reduce(bank Bank, to string) Money
 	Plus(addend Expression) Expression
 }
--- go/src/sum.go
+++ go/src/sum.go
@@ -9,11 +9,15 @@ func NewSum(augend Expression, addend Expression) Sum {
 	return Sum{augend: augend, addend: addend}
 }
 
+func (s Sum) Times(multiplier int) Expression {
+	return NewSum(s.augend.Times(multiplier), s.addend.Times(multiplier))
+}
+
 func (s Sum) Reduce(bank Bank, to string) Money {
 	amount := s.augend.Reduce(bank, to).amount + s.addend.Reduce(bank, to).amount
 	return Money{amount: amount, currency: to}
 }
 
 func (s Sum) Plus(addend Expression) Expression {
-	return Sum{augend: s.augend, addend: s.addend}
+	return Sum{augend: s, addend: addend}
 }
