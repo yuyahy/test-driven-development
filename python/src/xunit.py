e63ac4dad7816ef58bc3f78863741980b21b5de6
yuyahy <xvto.ya@gmail.com>
2024-12-16 12:19:59 +0000

    feat: 第20章完了

--- python/src/xunit.py
+++ python/src/xunit.py
@@ -5,34 +5,36 @@ class TestCase:
     def setUp(self):
         pass
 
+    def tearDown(self):
+        pass
+
     def run(self):
+        # fixture的な準備処理
         self.setUp()
+        # コンストラクタで指定されたメソッド名を取得 & 実行
         method = getattr(self,self.name)
         method()
+        # 後始末処理
+        self.tearDown()
 
 class WasRun(TestCase):
     def setUp(self):
-        self.wasRun=None
-        self.wasSetUp=1
+        self.log="setUp "
 
     def testMethod(self):
-        self.wasRun=1
+        self.log=self.log+"testMethod "
+
+    def tearDown(self):
+        self.log=self.log+"tearDown "
 
 class TestCaseTest(TestCase):
-    def setUp(self):
-        self.test = WasRun("testMethod")
-
-    def testRunning(self):
-        self.test.run()
-        assert(self.test.wasRun)
-
-    def testSetUp(self):
-        self.test.run()
-        assert(self.test.wasSetUp)
+    def testTemplateMethod(self):
+        test=WasRun("testMethod")
+        test.run()
+        assert(test.log=="setUp testMethod tearDown ")
 
 def main():
-    TestCaseTest("testRunning").run()
-    TestCaseTest("testSetUp").run()
+    TestCaseTest("testTemplateMethod").run()
 
 if __name__ == "__main__":
     main()
