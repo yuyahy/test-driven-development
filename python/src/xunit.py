f9a056c073c9ca0b6b4c665fcd98ed046634fe97
yuyahy <xvto.ya@gmail.com>
2024-12-16 21:42:40 +0900

    feat: 第20章完了 (#19)
    * feat: 第20章完了
    
    * ci: pythonのlinter workflowを追加
    
    * ci: workflowの修正
    
    * ci: workflowの修正
    
    * ci: workflowのキック
    
    * ci: workflowの修正
    
    * ci: workflowの修正
    
    * Apply black formatting [skip ci]
    
    ---------
    
    Co-authored-by: GitHub Actions <actions@github.com>

--- python/src/xunit.py
+++ python/src/xunit.py
@@ -1,38 +1,45 @@
 class TestCase:
-    def __init__(self,name):
+    def __init__(self, name):
         self.name = name
 
     def setUp(self):
         pass
 
+    def tearDown(self):
+        pass
+
     def run(self):
+        # fixture的な準備処理
         self.setUp()
-        method = getattr(self,self.name)
+        # コンストラクタで指定されたメソッド名を取得 & 実行
+        method = getattr(self, self.name)
         method()
+        # 後始末処理
+        self.tearDown()
+
 
 class WasRun(TestCase):
     def setUp(self):
-        self.wasRun=None
-        self.wasSetUp=1
+        self.log = "setUp "
 
     def testMethod(self):
-        self.wasRun=1
+        self.log = self.log + "testMethod "
+
+    def tearDown(self):
+        self.log = self.log + "tearDown "
+
 
 class TestCaseTest(TestCase):
-    def setUp(self):
-        self.test = WasRun("testMethod")
+    def testTemplateMethod(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert test.log == "setUp testMethod tearDown "
 
-    def testRunning(self):
-        self.test.run()
-        assert(self.test.wasRun)
-
-    def testSetUp(self):
-        self.test.run()
-        assert(self.test.wasSetUp)
 
 def main():
-    TestCaseTest("testRunning").run()
-    TestCaseTest("testSetUp").run()
+    #
+    TestCaseTest("testTemplateMethod").run()
+
 
 if __name__ == "__main__":
     main()
